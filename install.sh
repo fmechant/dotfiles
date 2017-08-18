c71a694e7ab2bff8a165dc87ed7f0f388586b344
fmechant <fmechant@bitimprove.eu>
2017-08-18 20:12:17 +0200

    initial version of install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Install all tools and Applications
+#source install-brew-and-cask.sh
+#source brew.sh
+#source brew-cask.sh
+#source dock.sh
+
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.config ~/.config
+ln -s ~/dotfiles/.gitignore ~/.gitignore
+ln -s ~/dotfiles/.hammerspoon ~/.hammerspoon
