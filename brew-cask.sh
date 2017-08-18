69837538e890b78dff42dd14f2ecf11b202a09a9
fmechant <fmechant@bitimprove.eu>
2017-08-18 17:38:09 +0200

    Added doc to mark manual actions needed

--- brew-cask.sh
+++ brew-cask.sh
@@ -6,24 +6,27 @@ brew update
 brew upgrade
 
 
+brew tap caskroom/fonts
+
+
 brew cask install iterm2
-brew cask install alfred
+brew cask install alfred  # Enable accessibility access manually
 brew cask install atom
-brew cask install dbeaver-community
+brew cask install java dbeaver-community
 brew cask install flux
 brew cask install font-cabin-sketch
 brew cask install geektool
 brew cask install gpgtools
-brew cask install hammerspoon
+brew cask install hammerspoon  # Enable accessibility access manually
 brew cask install karabiner-elements
 brew cask install firefox
-brew cask install lastpass
+brew cask install lastpass  # Run installer manually
 brew cask install fantastical
 brew cask install hipchat
 brew cask install vlc
 brew cask install android-file-transfer
 brew cask install calibre
-brew cask install blue-jeans-launcher
+brew cask install blue-jeans-launcher  # Run installer manually
 
 
 # brew cask install java sts
